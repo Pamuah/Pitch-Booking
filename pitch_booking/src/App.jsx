54bd53fbdc66693f124f8c7d6499dc82b1ec681e
KobbyAdom <showamankwah6655@gmail.com>
2025-06-12 09:11:15 +0000

    admin

--- pitch_booking/src/App.jsx
+++ pitch_booking/src/App.jsx
@@ -3,6 +3,13 @@ import LandingPage from "./pages/landingPage";
 import HomePage from "./pages/homepage";
 import BookingPage from "./pages/bookingPage";
 import AdminLoginPage from "./pages/AdminLoginPage";
+import AdminDashboard from "./pages/AdminDashboard";
+import MainAdminPage from "./pages/mainadminpage";
+import ManagePitches from "./pages/ManagePitches";
+import BookingsPage from "./pages/BookingsPage";
+import ReviewsPage from "./pages/ReviewsPage";
+import SettingsPage from "./pages/SettingsPage";
+
 
 function App() {
   return (
@@ -13,9 +20,15 @@ function App() {
         <Route path="/landingPage" element={<LandingPage />} />
         <Route path="/bookingPage" element={<BookingPage />} />
         <Route path="/adminlogin" element={<AdminLoginPage />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/mainadmin" element={<MainAdminPage />} />
+        <Route path="/admin/pitches" element={<ManagePitches />} />
+        <Route path="/admin/bookings" element={<BookingsPage />} />
+        <Route path="/admin/reviews" element={<ReviewsPage />} />
+        <Route path="/admin/settings" element={<SettingsPage />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
+ export default App;
